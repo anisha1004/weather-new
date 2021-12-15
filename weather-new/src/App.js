fc12fab71bec0b3c181e2ed86ac724bffbbcdb40
anisha1004 <54628217+anisha1004@users.noreply.github.com>
2021-12-15 10:40:29 +0530

    first

---
+++ weather-new/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Weather from "./pages/Weather.js";
+
+function App() {
+	return (
+		<div className='app'>
+			<Weather />
+		</div>
+	);
+}
+
+export default App;
---
+++ weather-new/src/pages/Weather.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "../css/Weather.css";
+import SearchIcon from "../images/searchIcon.svg";
+import Icon from "../images/mid-icon.svg";
+
+const Weather = () => {
+	return (
+		<div className='weather'>
+			<div className='weather-left'>
+				<div className='top'>
+					<span>WEATHER APP</span>
+					<span>BROWSE MAP</span>
+				</div>
+				<div className='middle'>
+					<div className='search'>
+						<input className='searchbar' />
+						<i>
+							<img src={SearchIcon} alt='search' />
+						</i>
+					</div>
+					<div>
+						<img src={Icon} className='icon' />
+					</div>
+					<div className='temp'>27&deg;</div>
+
+					<div className='date'>14 June 2021</div>
+					<div className='day-time'>
+						<span>Thrusday | </span>
+						<span>2:45AM</span>
+					</div>
+				</div>
+				<div className='strip'></div>
+				<div className='forecast'></div>
+			</div>
+			<div className='weather-right'></div>
+		</div>
+	);
+};
+
+export default Weather;
